75646812d4cd00cf1b39e8c820f44f384147e3ca
Felipe Carvalho <45293395+FelipeCarvalho27@users.noreply.github.com>
2021-02-08 12:32:23 -0300

    Primeiros passos

---
+++ Sisteminha/Sisteminha/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sisteminha.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly  SisteminhaEntities db;
+        public HomeController()
+        {
+            db = new SisteminhaEntities();
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string nome, string sobrenome, string datanascimento, string fone, string email)
+        {
+            cadastro Novo = new cadastro();
+            Novo.nome = nome;
+            Novo.sobrenome = sobrenome;
+            Novo.dataNasc = Convert.ToDateTime(datanascimento);
+            Novo.telefone = fone;
+            Novo.email = email;
+            db.cadastro.Add(Novo);
+            db.SaveChanges();
+
+
+            return RedirectToAction("About");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Sisteminha/Sisteminha/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sisteminha.MvcApplication" Language="C#" %>
